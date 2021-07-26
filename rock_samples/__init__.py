9017a7913aa5655c164c1c018108259f77ab1b91
Quelich <emre1kilic@outlook.com>
2021-07-26 23:27:31 +0300

    backup - got a weird value error

--- rock_samples/__init__.py
+++ rock_samples/__init__.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.metrics import acc
+
 from utilities import _getClasses_
 from utilities import _plotImages_
 from utilities import _retrieveBatches_
@@ -38,18 +40,78 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 )
 
 # Visualizing the data
-_plotImages_(train_ds)
-_retrieveBatches_(train_ds)
+# _plotImages_(train_ds)
+# _retrieveBatches_(train_ds)
 
-AUTOTUNE = tf.data.Dataset.AUTOTUNE
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-# Standardize values
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+# Standardize values in range [0,1] by using Rescaling layer
+normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image))
+
+# ML MODEL
+
+NUM_CLASSES = 1
+
+# The machine learning model named in honour of James Hutton
+hutton = Sequential([
+    layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(NUM_CLASSES)
+])
+
+# Model Compilation
+
+hutton.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+# Model Summary
+hutton.summary()
+
+# Model Training
+epochs = 10
+history = hutton.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+
+# Visualize training results
+cc = np.array(history.history['accuracy'])
+val_acc = np.array(history.history['val_accuracy'])
+
+loss = np.array(history.history['loss'])
+val_loss = np.array(history.history['val_loss'])
+
+epochs_range = np.array(range(epochs))
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
