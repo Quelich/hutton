eb94dcb88863a9b96284e197629efcc51049c2d8
Quelich <emre1kilic@outlook.com>
2021-07-26 22:59:15 +0300

    created dataset for andesite class

--- rock_samples/__init__.py
+++ rock_samples/__init__.py
@@ -1,3 +1,55 @@
-"""rock_samples dataset."""
+# Creating the dataset of specified directory
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from utilities import _getClasses_
+from utilities import _plotImages_
+from utilities import _retrieveBatches_
 
-from .rock_samples import RockSamples
+# Data PARAMETERS
+BATCH_SIZE = 32
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+# Data directory
+data_dir = 'D:/GitRepos/hutton/rock_samples/train/'
+
+# Training 80% of the images
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+# Validating 20% of the images
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+
+# Visualizing the data
+_plotImages_(train_ds)
+_retrieveBatches_(train_ds)
+
+AUTOTUNE = tf.data.Dataset.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Standardize values
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
