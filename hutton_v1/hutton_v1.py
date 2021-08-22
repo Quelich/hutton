04375b4c530fc920ba4ad8ed04db3a9983f5f681
Quelich <emre1kilic@outlook.com>
2021-08-23 01:30:19 +0300

    created a class for Hutton model

--- hutton_v1/hutton_v1.py
+++ hutton_v1/hutton_v1.py
@@ -8,151 +8,62 @@ from tensorflow.keras.models import Sequential
 from hutton_utilities import _visualizeData_
 from hutton_utilities import _visualizeAugmentedData_
 from hutton_utilities import _logResults_
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, experimental
+from tensorflow.keras import Model
 
-# Data PARAMETERS
-BATCH_SIZE = 3
-IMG_HEIGHT = 180
-IMG_WIDTH = 180
-# Data directory
-data_dir = 'D:/GitRepos/hutton/rock_samples/train'
+class Hutton(Model):
+    BATCH_SIZE = 3
+    NUM_CLASSES = 3
+    IMG_HEIGHT = 180
+    IMG_WIDTH = 180
 
-# Training 80% of the images
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=BATCH_SIZE,
-)
-# Validating 20% of the images
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=BATCH_SIZE
-)
+    def __init__(self, batch_size, num_classes, img_height, img_width):
+        super(Hutton, self).__init__()
+        self.BATCH_SIZE = batch_size
+        self.NUM_CLASSES = num_classes
+        self.IMG_HEIGHT = img_height
+        self.IMG_WIDTH = img_width
+        self.preprocessing = experimental.preprocessing.Rescaling(1. / 255,
+                                                                  input_shape=(self.IMG_HEIGHT, self.IMG_WIDTH, 3))
+        self.conv1 = Conv2D(2, 3, padding='same', activation='relu')
+        self.pooling1 = MaxPooling2D()
+        self.conv2 = Conv2D(4, 3, padding='same', activation='relu')
+        self.pooling2 = MaxPooling2D()
+        self.dropout1 = Dropout(0.1)
+        self.conv3 = Conv2D(8, 3, padding='same', activation='relu')
+        self.pooling3 = MaxPooling2D()
+        self.dropout2 = Dropout(0.2)
+        self.flatten = Flatten()
+        self.d1 = Dense(4, activation='relu',
+                        kernel_regularizer=tf.keras.regularizers.l2(0.01))
+        self.d2 = Dense(self.NUM_CLASSES)
 
-# Displaying the batch information
-# print("Train dataset batches")
-# _retrieveBatches_(train_ds)
-# print("Validation dataset batches")
-# _retrieveBatches_(val_ds)
+    def get_NUM_CLASSES(self):
+        return self.NUM_CLASSES
 
-class_names = train_ds.class_names
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    def set_NUM_CLASSES(self, new_class_value):
+        self.NUM_CLASSES = new_class_value
 
-# Standardize values in range [0,1] by using Rescaling layer
-normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
+    def get_BATCH_SIZE(self):
+        return self.BATCH_SIZE
 
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# # Notice the pixels values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
+    def set_BATCH_SIZE(self, new_batch_size):
+        self.BATCH_SIZE = new_batch_size
 
-# ML MODEL ! --------------------------------------------------MODEL
-# ----------------------------------------------------------------------- TODO Initialize Report File (PDF?) every
-#  process should be documented into a report file
-NUM_CLASSES = 3
+    def get_IMG_HEIGHT(self):
+        return self.IMG_HEIGHT
 
-# The machine learning model named in honour of James Hutton
-hutton = Sequential([
-    layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-    layers.Conv2D(2, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(4, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.1),
-    layers.Conv2D(8, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.2),
-    layers.Flatten(),
-    layers.Dense(4, activation='relu',
-                 kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    layers.Dense(NUM_CLASSES)
-])
-# Model Optimization
-adam = tf.keras.optimizers.Adam(learning_rate=0.00008,
-                                beta_1=0.9,
-                                beta_2=0.999,
-                                amsgrad=False)
+    def set_IMG_HEIGHT(self, new_img_height):
+        self.IMG_HEIGHT = new_img_height
 
-# Model Compilation
+    def get_IMG_WIDTH(self):
+        return self.IMG_WIDTH
 
-hutton.compile(
-    optimizer=adam,
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+    def set_IMG_WIDTH(self, new_img_width):
+        self.IMG_WIDTH = new_img_width
 
-# Model Summary
-hutton.summary()
+    def call(self, inputs, training=None, mask=None):
+        pass
 
-# Model Training
-epochs = 25
-history = hutton.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
-
-# Visualize training results
-_visualizeData_(history, epochs)
-plt.show()
-
-# Fixing Overfitting - Data Augmentation
-data_augmentation = keras.Sequential(
-    [
-        layers.experimental.preprocessing.RandomFlip("horizontal",
-                                                     input_shape=(IMG_HEIGHT,
-                                                                  IMG_WIDTH,
-                                                                  3)),
-        layers.experimental.preprocessing.RandomRotation(0.1),
-        layers.experimental.preprocessing.RandomZoom(0.1),
-    ]
-)
-# Visualizing the augmented images
-_visualizeAugmentedData_(train_ds, data_augmentation)
-
-# Compile the model Hutton
-hutton.compile(optimizer='adam',
-               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-               metrics=['accuracy'])
-
-hutton.summary()
-
-history = hutton.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
-# Visualize training results
-_visualizeData_(history, epochs)
-
-test_dir = 'D:/GitRepos/hutton/rock_samples/test/granite/79.jpg'
-
-# TODO use for loop for multiple image files
-test_img = keras.preprocessing.image.load_img(
-    test_dir, target_size=(IMG_HEIGHT, IMG_WIDTH)
-)
-
-img_array = keras.preprocessing.image.img_to_array(test_img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
-
-predictions = hutton.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-# Displaying the results
-results = "This image most likely belongs to {} with a {:.2f} percent confidence".format(class_names[np.argmax(score)],
-                                                                                         100 * np.max(score))
-print(results)
-
-# Logging the results
-output_data = results + ";for " + test_dir
-
-_logResults_(output_data)
+    def get_config(self):
+        pass
